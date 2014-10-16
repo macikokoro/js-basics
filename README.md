de1369b55624a0491b83d7b63180570c9d73092c
macikokoro <betwinsouls@gmail.com>
2014-10-16 10:33:34 -0700

    adding starting point, a simple adding calculator

---
+++ README.md
@@ -0,0 +1,5 @@
+# JS Basics
+
+Repo with JS basic programs for beginners.
+
+License: MIT
---
+++ js-calculator/app.js
@@ -0,0 +1,10 @@
+function add(){
+var a,b,c;
+a=Number(document.getElementById("first").value);
+b=Number(document.getElementById("second").value);
+
+function c (a, b) {
+    return "result: " + (a + b);
+}
+document.getElementById("answer").innerHTML = c(a, b);
+}
